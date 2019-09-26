f4301cbb52d896fcf8f4024184953fdbd04f30fb
Yeron <53163857+yeronReekoh@users.noreply.github.com>
2019-09-26 11:29:02 +1000

    test
    test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start</h2>
     </div>
   );
 }
